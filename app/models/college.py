40ffa7246b818b3605d3b7bbcaa344ac478807d0
Bryant Lee <bl692@cornell.edu>
2020-06-21 21:34:30 -0400

    fixed font overflowing

--- app/models/college.py
+++ app/models/college.py
@@ -520,7 +520,7 @@ class College(db.Model):
             ivy_leagues = {'Cornell University', 'Dartmouth University', 'Brown University', 'Columbia University',
                 'University of Pennsylvania', 'Princeton University', 'Yale University', 'Harvard University'}
 
-            if college.description == '':
+            if college.description:
                 default = (college.institution_type).capitalize() + ' Instiution in ' + college.school_state
                 college.description = 'Ivy League Institution' if college.name in ivy_leagues else default   
                 
