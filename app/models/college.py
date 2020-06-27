f2fb2509f0f651b5265823449f60b706c93122fa
Bryant Lee <bl692@cornell.edu>
2020-06-26 21:38:54 -0400

    refactored scattergram CSV upload backend, added data validation

--- app/models/college.py
+++ app/models/college.py
@@ -538,7 +538,7 @@ class College(db.Model):
             'University of Chicago', 'Amherst College', 'Williams College',
             'Massachusetts Institute of Technology',
             'Georgia Institute of Technology',
-            'California Institute of Technology', 'Duke University'
+            'California Institute of Technology', 'Duke University', 'Pitzer College'
         }
         early_deadlines = [
             datetime(2017, 11, 4),
