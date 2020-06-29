9216250247cd436ee73565f48a9a715bf6ff7e4e
Bryant Lee <bl692@cornell.edu>
2020-06-29 09:15:13 -0400

    nothing to see here

--- app/static/scripts/footer.js
+++ app/static/scripts/footer.js
@@ -6,6 +6,21 @@ function toggleLanguage(){
   if(isToggled)
     $('#language_footer').transition('jiggle'); //or pulse
 }
-// $('.autumn.leaf')
-//   .transition('jiggle')
-// ;
+
+// nothing to see here
+let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13].reverse();
+let keysToPress = konamiCode.slice();
+document.onkeydown = checkKey;
+function checkKey(e) {  
+  e = e || window.event;
+  if(e.keyCode == keysToPress[keysToPress.length - 1]){
+    keysToPress.pop();
+    if (keysToPress.length == 0){
+      confetti.toggle();
+      keysToPress = konamiCode.slice();
+    }
+  }
+  else{
+    keysToPress = konamiCode.slice();
+  }
+}
