a663a92f847c97ebc8005d05cb31d06739019eb1
Bryant Lee <bl692@cornell.edu>
2020-07-02 08:35:34 -0400

    nothing to see here pt2

--- app/static/scripts/footer.js
+++ app/static/scripts/footer.js
@@ -8,19 +8,43 @@ function toggleLanguage(){
 }
 
 // nothing to see here
-let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13].reverse();
-let keysToPress = konamiCode.slice();
-document.onkeydown = checkKey;
-function checkKey(e) {  
-  e = e || window.event;
-  if(e.keyCode == keysToPress[keysToPress.length - 1]){
-    keysToPress.pop();
-    if (keysToPress.length == 0){
-      confetti.toggle();
+$(document).ready(function() {
+  let highScore = Cookies.get('highScore');
+  if(highScore == null){
+    Cookies.set('highScore', 99999, { expires: 365, sameSite: 'secure' });
+  }
+  let start = performance.now();
+  let end = 0;
+  let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13].reverse();
+  let keysToPress = konamiCode.slice();
+  document.onkeydown = checkKey;
+  function checkKey(e) {  
+    e = e || window.event;
+    if(e.keyCode === keysToPress[keysToPress.length - 1]){
+      if(keysToPress.length == konamiCode.length){
+        start = performance.now();
+      }
+      keysToPress.pop();
+      if (keysToPress.length === 0){
+        end = performance.now();
+        let totalTime = (end - start)/1000;
+        if(totalTime < highScore || highScore == null){
+          highScore = totalTime;
+          Cookies.set('highScore', highScore, { expires: 365 });
+          activateConfetti();
+        }
+        alert('You took ' + totalTime + ' seconds to type in the Konami Code.\nHighscore: ' + highScore);
+        keysToPress = konamiCode.slice();
+      }
+    }
+    else{
       keysToPress = konamiCode.slice();
     }
   }
-  else{
-    keysToPress = konamiCode.slice();
-  }
+});
+
+async function activateConfetti() {
+  confetti.start();
+  await new Promise(r => setTimeout(r, 3000));
+  confetti.stop();
 }
