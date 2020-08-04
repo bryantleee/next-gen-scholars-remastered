a26a8694a2740eab5d02e91d18ffb08cd6d709e3
Bryant Lee <bl692@cornell.edu>
2020-08-04 13:21:46 -0400

    added luminescence calculating functions

--- app/models/miscellaneous.py
+++ app/models/miscellaneous.py
@@ -163,3 +163,60 @@ def get_easter_egg_emoji(college_name):
         'Brown University' : '🐻'
     }
     return easter_eggs.get(college_name)
+
+def calculate_luminescence(r, g, b):
+    '''
+    - r: int 
+        red color between 0 and 255
+    - g: int
+        green color between 0 and 255
+    - b: int
+        blue color between 0 and 255
+    
+    Calculates luminance of a, returns a single float between 0 and 1 that represents luminescence
+    
+    See this page for more info:
+    https://www.w3.org/TR/WCAG20/#contrast-ratiodef
+    '''
+    colors = (r/255, g/255, b/255)
+    final_colors = tuple(color/12.92 if color <= 0.03928 else ((color + 0.055) / 1.055) ** 2.4 for color in colors)
+    return 0.2126 * final_colors[0] + 0.7152 * final_colors[1] + 0.0722 * final_colors[2]
+
+
+def calculate_luminescence_ratio(color1, color2, color_format='hex'):
+    '''
+    - color1: 
+        tuple of (r: int, g: int, b: int) OR
+        tuple of (r: int, g: int, b: int, a: int) OR
+        str of hex format '#XXXXXX' OR 'XXXXXX'
+            represents the first color you want to check 
+    - color2: 
+        tuple of (r: int, g: int, b: int) OR
+        tuple of (r: int, g: int, b: int, a: int) OR
+        str of hex format '#XXXXXX' OR 'XXXXXX'
+            represents the second color you want to check 
+    
+    color_format: str of either {'hex', 'rgba', or 'rgb'}
+        describes the color format of color1 and color2
+        both color1 and color2 should be the same color format
+    
+    Calculates luminescence ratio between two colors, outputs a float between 0 and 21.
+    The higher the number, the better. 
+    Ideally should be:
+        ≥ 3.0 for large text  
+        ≥ 4.5 for everything else
+    
+    See this page for more info:
+    https://www.w3.org/TR/WCAG20/#contrast-ratiodef
+    '''
+    color_luminescences = []
+    for color in (color1, color2):
+        if color_format == 'hex':
+            color = (lambda x : tuple(int(x.lstrip('#')[i:i+2], 16) for i in (0, 2, 4)))(color)
+        elif color_format == 'rgba':
+            color = color[-1]
+        color_luminescences.append(calculate_luminescence(color[0], color[1], color[2]))
+    
+    ratio1 = (color_luminescences[0] + 0.05) / (color_luminescences[1] + 0.05)
+    ratio2 = (color_luminescences[1] + 0.05) / (color_luminescences[0] + 0.05)
+    return max(ratio1, ratio2)
